a896b39687eb5205cf4b7772d3b6841137c79c5b
hamza mlayeh <hamza@outlook.com>
2018-10-01 11:59:14 +0100

    Initial commit

---
+++ app/src/main/java/com/orvea/asus/orvea/SplashScreen.java
@@ -0,0 +1,48 @@
+package com.orvea.asus.orvea;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static int SPLASH_TIME_OUT=3000;
+    Animation animFadeIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        TextView logo = findViewById(R.id.logo);
+
+
+        animFadeIn= AnimationUtils.loadAnimation(this, R.anim.translate);
+
+        logo.setAnimation(animFadeIn);
+        new Handler().postDelayed(new Runnable() {
+
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
