2adbec6f4b95fef0587c998df96d0cd8d2aa1882
khaled <khaledhizawi@gmail.com>
2018-10-02 10:25:40 +0100

    home

--- app/src/main/java/com/orvea/asus/orvea/HomeActivity.java
+++ app/src/main/java/com/orvea/asus/orvea/HomeActivity.java
@@ -3,6 +3,7 @@ package com.orvea.asus.orvea;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,6 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+    }
+
+    public void connexion(View view) {
         Intent intent= new Intent(this,login.class);
         startActivity(intent);
     }
