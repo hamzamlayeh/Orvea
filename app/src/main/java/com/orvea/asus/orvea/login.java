0b66fbee3759d8986ec254b683d16e80217e5844
khaled <khaledhizawi@gmail.com>
2018-10-02 09:49:06 +0100

    test comit

--- app/src/main/java/com/orvea/asus/orvea/login.java
+++ app/src/main/java/com/orvea/asus/orvea/login.java
@@ -1,16 +1,13 @@
 package com.orvea.asus.orvea;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        Intent intent= new Intent(this,login.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_login);
     }
 }
