6f5300848b1afd8f8ac8964c85dac2bac6156cd8
hamza mlayeh <hamza@outlook.com>
2018-10-12 12:22:41 +0100

    login/inscrir

--- app/src/main/java/com/orvea/asus/orvea/InscriptionActivity.java
+++ app/src/main/java/com/orvea/asus/orvea/InscriptionActivity.java
@@ -1,13 +1,66 @@
 package com.orvea.asus.orvea;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class InscriptionActivity extends AppCompatActivity {
+
+    EditText Nom, Prenom, Email, Password, Tel;
+    String email, password, nom, prenom, tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+
+        Email = findViewById(R.id.email);
+        Password = findViewById(R.id.pass);
+        Nom = findViewById(R.id.nom);
+        Prenom = findViewById(R.id.prenom);
+        Tel = findViewById(R.id.tel);
+    }
+
+    public void valider(View view) {
+        email = Email.getText().toString().trim();
+        password = Password.getText().toString().trim();
+        nom = Password.getText().toString().trim();
+        prenom = Password.getText().toString().trim();
+        tel = Password.getText().toString().trim();
+
+        if (valider()) {
+            startActivity(new Intent(this, login.class));
+        }
+    }
+
+    private boolean valider() {
+        boolean valide = true;
+        if (nom.isEmpty()) {
+            Nom.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (prenom.isEmpty()) {
+            Prenom.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (tel.isEmpty()) {
+            Tel.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (email.isEmpty()) {
+            Email.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (!email.isEmpty() && (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())) {
+            Email.setError(getString(R.string.email_invalide));
+            valide = false;
+        }
+        if (password.isEmpty()) {
+            Password.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        return valide;
     }
 }
--- app/src/main/java/com/orvea/asus/orvea/login.java
+++ app/src/main/java/com/orvea/asus/orvea/login.java
@@ -1,19 +1,47 @@
 package com.orvea.asus.orvea;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
+
+    EditText Email, Password;
+    String email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Email = findViewById(R.id.email);
+        Password = findViewById(R.id.passe);
     }
 
     public void valider(View view) {
-        startActivity(new Intent(this,ListesMembresActivity.class));
+        email = Email.getText().toString().trim();
+        password = Password.getText().toString().trim();
+
+        if (valider()) {
+            startActivity(new Intent(this, ListesMembresActivity.class));
+        }
+    }
+
+    private boolean valider() {
+        boolean valide = true;
+        if (email.isEmpty()) {
+            Email.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (!email.isEmpty() && (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())) {
+            Email.setError(getString(R.string.email_invalide));
+            valide = false;
+        }
+        if (password.isEmpty()) {
+            Password.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        return valide;
     }
 }
